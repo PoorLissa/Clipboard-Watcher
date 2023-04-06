3ddc68f7f2ce5d851c3df856296bcf8b075a4ffd
pm <pm@CM33L63.com>
2023-04-06 10:51:14 -0400

    Add project files.

---
+++ Clipboard Watcher/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Clipboard_Watcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bClear = new System.Windows.Forms.Button();
+            this.bCopy = new System.Windows.Forms.Button();
+            this.tbFilter = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(879, 1114);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "lalala putin is khuilo!";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(879, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(4, 1114);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.bClear);
+            this.panel1.Controls.Add(this.bCopy);
+            this.panel1.Controls.Add(this.tbFilter);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(883, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(797, 1114);
+            this.panel1.TabIndex = 2;
+            // 
+            // bClear
+            // 
+            this.bClear.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bClear.Location = new System.Drawing.Point(22, 338);
+            this.bClear.Name = "bClear";
+            this.bClear.Size = new System.Drawing.Size(748, 200);
+            this.bClear.TabIndex = 2;
+            this.bClear.Text = "Clear All";
+            this.bClear.UseVisualStyleBackColor = true;
+            this.bClear.Click += new System.EventHandler(this.bClear_Click);
+            // 
+            // bCopy
+            // 
+            this.bCopy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bCopy.Location = new System.Drawing.Point(22, 101);
+            this.bCopy.Name = "bCopy";
+            this.bCopy.Size = new System.Drawing.Size(748, 200);
+            this.bCopy.TabIndex = 1;
+            this.bCopy.Text = "Copy All";
+            this.bCopy.UseVisualStyleBackColor = true;
+            this.bCopy.Click += new System.EventHandler(this.bCopy_Click);
+            // 
+            // tbFilter
+            // 
+            this.tbFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFilter.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbFilter.Location = new System.Drawing.Point(22, 31);
+            this.tbFilter.Name = "tbFilter";
+            this.tbFilter.PlaceholderText = "Filter";
+            this.tbFilter.Size = new System.Drawing.Size(748, 40);
+            this.tbFilter.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1680, 1114);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.richTextBox1);
+            this.MinimumSize = new System.Drawing.Size(1200, 630);
+            this.Name = "Form1";
+            this.Text = "-= Clipboard Watcher =-";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbFilter;
+        private System.Windows.Forms.Button bClear;
+        private System.Windows.Forms.Button bCopy;
+    }
+}
+
---
+++ Clipboard Watcher/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#pragma warning disable IDE1006
+
+namespace Clipboard_Watcher
+{
+    public partial class Form1 : Form
+    {
+        private readonly System.Threading.Thread th = null;
+
+        private static string oldText = "";
+        private static string newText = "";
+        private static bool isRunning = false;
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (th == null)
+            {
+                isRunning = true;
+
+                th = new System.Threading.Thread(threadFunc)
+                {
+                    IsBackground = true,                                        // Does not prevent the app from terminating
+                    Priority = System.Threading.ThreadPriority.Normal,
+                    Name = nameof(threadFunc)
+                };
+
+                th.SetApartmentState(System.Threading.ApartmentState.STA);
+                th.Start();
+            }
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        // Copy all the text to clipboard
+        private void bCopy_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length > 0)
+            {
+                oldText = richTextBox1.Text;
+
+                Clipboard.SetText(oldText);
+            }
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        // Clear all the text
+        private void bClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+
+        // Main thread func
+        private void threadFunc()
+        {
+            // Prevent posting the text that is already in the Clipboard when the app starts
+            if (System.Windows.Forms.Clipboard.ContainsText())
+            {
+                oldText = Clipboard.GetText();
+            }
+
+            while (isRunning)
+            {
+                if (System.Windows.Forms.Clipboard.ContainsText())
+                {
+                    newText = Clipboard.GetText();
+
+                    if (tbFilter.Text.Length == 0 || newText.Contains(tbFilter.Text))
+                    {
+                        if (newText != oldText)
+                        {
+                            oldText = newText;
+
+                            richTextBox1.Invoke(new MethodInvoker(delegate
+                            {
+
+                                richTextBox1.AppendText(newText);
+                                richTextBox1.AppendText(Environment.NewLine);
+
+                            }));
+                        }
+                    }
+                }
+
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+    }
+}
+
+#pragma warning restore IDE1006
